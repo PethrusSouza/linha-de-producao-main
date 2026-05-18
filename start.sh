d0da7a6814563017b63f6cb61a8ad70a916204ad
Pethrus Souza <168316545+PethrusSouza@users.noreply.github.com>
2026-05-17 23:49:37 -0300

    Melhorar start do Render

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python manage.py migrate
-gunicorn sistema_os.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+echo "Aplicando migrations..."
+python manage.py migrate --noinput
+
+echo "Iniciando Gunicorn na porta ${PORT:-8000}..."
+exec python -m gunicorn sistema_os.wsgi:application --bind 0.0.0.0:${PORT:-8000} --log-file -
